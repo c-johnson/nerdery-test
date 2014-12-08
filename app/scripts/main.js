33844f528bb23c3f4e86eac27d011c46f480b6d4
Chris Johnson <chris@cjohnson.io>
2014-12-08 14:28:06 -0600

    Carousel auto-rotation working great!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,10 @@
 
   var MainController = function () {
     this.carouselActive = 1;
+    this.carouselRotateSpeed = 5000;  // (once every 5 seconds);
+    this.carouselHitboxInterval = 100;  // How often we check the hitbox -- can be increased for more performance
+    this.debugMode = false;
+    this.maxSlides = 4;
 
     this.init = function () {
       this.registerElements();
@@ -32,16 +36,32 @@
       this.carouselSlides.filter('[data-slide="'+id+'"]').addClass('carousel-active');
     };  
 
+    /* This function registers a timer, which rotates the carousel's active slide every 5 seconds (default) the cursor remains outside the carousel's hitbox */
     this.registerCarouselTimer = function () {
-      setInterval(function () {
-        var isHovered = this.carousel.is(":hover");
-        console.log('cursor in hitbox? : ' + isHovered);
-      }.bind(this), 300);
+      this.carouselRotateTimer = 0;
+
+      this.carouselTimer = setInterval(function () {
+        var carouselHovered = this.carousel.is(":hover");
+        this.carouselRotateTimer = (carouselHovered) ? 0 : this.carouselRotateTimer + this.carouselHitboxInterval;
+        if (this.debugMode) {
+          console.log('Time since last rotate = ' + this.carouselRotateTimer);
+        }
+
+        if (this.carouselRotateTimer > this.carouselRotateSpeed) {
+          this.rotateSlides();
+          this.carouselRotateTimer = 0;
+        }
+      }.bind(this), this.carouselHitboxInterval);
     };
 
-    this.rotateSlides = function () {
+    this.stopCarouselTimer = function () {
+      clearInterval(this.carouselTimer);
+    }
 
-    };  
+    this.rotateSlides = function () {
+      var activeThumbID = this.carouselThumbs.filter('.active').attr('data-slide-id');
+      this.activateCarousel(parseInt(activeThumbID) + 1);
+    };
   };
 
   var mainCtrl = new MainController();
