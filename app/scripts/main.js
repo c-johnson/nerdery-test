d7618910909f99b0d7e9489dd05eaba6e97c1738
Chris Johnson <chris@cjohnson.io>
2014-12-08 14:10:38 -0600

    Slider rotation works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,6 +11,7 @@
 
     this.registerElements = function () {
       this.carouselThumbs = $('[data-carousel-thumbs] [data-slide-id]');
+      this.carouselSlides = $('[data-carousel-slides] [data-slide]');
     };
 
     this.registerHandlers = function () {
@@ -24,6 +25,9 @@
     this.activateCarousel = function (id) {
       this.carouselThumbs.removeClass('active');
       this.carouselThumbs.filter('[data-slide-id="'+id+'"]').addClass('active');
+
+      this.carouselSlides.removeClass('carousel-active');
+      this.carouselSlides.filter('[data-slide="'+id+'"]').addClass('carousel-active');
     };  
 
     this.registerTabs = function () {
